905463ab8f18ca359473f8d104bdce10ac9408e2
Tommy Le <TaiLe2435@gmail.com>
2022-01-31 09:42:46 -0600

    Initial commit for the file

---
+++ SimplePend.cs
@@ -0,0 +1,47 @@
+/*============================================================================
+SimplePend.cs Defines a class for simulating a simple pendulum
+============================================================================*/
+using System;
+
+namespace Sim
+{
+    public class SimplePend
+    {
+        private double len = 1.1; // pendulum length
+        private double g = 9.81; // gravitational field strength
+        
+        /*====================================================================
+        constructor
+        ====================================================================*/
+        public SimplePend()
+        {
+            Console.WriteLine("Inside constructor");
+        }
+
+
+        /*====================================================================
+        Getters and Setters
+        ====================================================================*/
+        public double L
+        {
+            get {return(len);}
+
+            set
+            {
+                if(value > 0.0)
+                    len = value;
+            }
+        }
+        
+        public double G
+        {
+            get {return(g);}
+
+            set
+            {
+                if(value >= 0.0)
+                    g = value;
+            }
+        }
+    }
+}

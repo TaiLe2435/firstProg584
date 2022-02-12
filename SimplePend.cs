f1614452d7ab9820446f84dc8482caaa629a5c25
Tommy Le <TaiLe2435@gmail.com>
2022-02-12 16:35:00 -0600

    adding RK4 integrator

--- SimplePend.cs
+++ SimplePend.cs
@@ -19,7 +19,8 @@ namespace Sim
         public SimplePend()
         {
             x = new double[n];
-            f = new double[n];
+            // need another intermediate step
+            f = new double[n]; // need 2D array
 
             x[0] = 1.0;
             x[1] = 0.0;
@@ -28,19 +29,58 @@ namespace Sim
         /*====================================================================
         step: perform one integration step vua Euler's Method
         ====================================================================*/
-        public void step(double dt) //str method
+        public void step(double dt)
         {
             rhsFunc(x,f);
             int i;
             for(i=0;i<n;++i)
             {
-                //if(method ="euler")
-                //{
                 x[i] = x[i] + f[i]*dt;
-                //}
-
             }
             // Console.WriteLine($"{f[0].ToString()}   {f[1].ToString()}");
+        }
+        /*====================================================================
+        RK4: Numerically integrates for one time step using RK4
+        ====================================================================*/
+        public void RK4(double dt)
+        {
+            double[,] k = new double[4,2] {{0.0 ,0.0},{0.0 ,0.0},{0.0 ,0.0},{0.0 ,0.0}};
+            double[] xi = new double[n];
+            int i;
+
+            //Solving for slope values
+            
+            rhsFunc(x,f);
+            for(i = 0; i < n; ++i)
+            {
+                k[0,i] = f[i]; //kA
+                xi[i] = x[i] + k[0,i]*0.5*dt;
+            }
+
+            rhsFunc(xi,f);
+            for(i = 0; i < n; ++i)
+            {   
+                k[1,i] = f[i]; //kB
+                xi[i] = x[i] + k[1,i]*0.5*dt;
+            }    
+
+            rhsFunc(xi,f);
+            for(i = 0; i < n; ++i)
+            {
+                k[2,i] = f[i]; //kC
+                xi[i] = x[i] + k[2,i]*dt;
+            }
+
+            rhsFunc(xi,f);
+            for(i = 0; i < n; ++i)
+            {
+                k[3,i] = f[i]; //kD
+            }
+
+            for(i = 0; i < n; ++i)
+            {
+            x[i] = x[i] + (k[0,i] + 2.0*k[1,i] + 2.0*k[2,i] + k[3,i])/6.0 * dt; //New state
+            }
         }
 
         /*====================================================================
